b31d9fe49aaea06776f5b0d62610a8328b594810
ChaneHyun <chanehyun47@gmail.com>
2022-09-29 15:48:12 +0900

    commit5

--- 입문/성적표.java
+++ 입문/성적표.java
@@ -3,6 +3,7 @@ package 입문;
 class 성적표 {
     public static void main(String[] args){
         성적표 solution = new 성적표();
+        System.out.println(solution.solution());
     }
 
     public String solution(){
--- 입문/소수구하기.java
+++ 입문/소수구하기.java
@@ -3,6 +3,7 @@ package 입문;
 class 소수구하기 {
     public static void main(String[] args){
         소수구하기 solution = new 소수구하기();
+        System.out.println(solution.solution());
     }
 
     public String solution(){
